de86482d3d0d23afb01a12896948f4fa35b4fddd
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-10-04 12:02:55 +0200

    Descripcion de los usuarios

--- src/main/java/madstodolist/controller/RegistradosController.java
+++ src/main/java/madstodolist/controller/RegistradosController.java
@@ -1,6 +1,8 @@
 package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSession;
+import madstodolist.controller.exception.TareaNotFoundException;
+import madstodolist.controller.exception.UsuarioNoLogeadoException;
 import madstodolist.model.Tarea;
 import madstodolist.model.Usuario;
 import madstodolist.model.UsuarioRepository;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,6 +28,15 @@ public class RegistradosController {
     public String usuariosRegistrados(Model model){
         List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
         model.addAttribute("usuarios", usuarios);
+        return "registrados";
+    }
+
+    @GetMapping("/registrados/:{id}")
+    public String usuariosRegistradosId(@PathVariable(value = "id") Long idUsuario, Model model){
+        Usuario usuario = usuarioService.findById(idUsuario);
+        if (usuario == null) {
+            throw new UsuarioNoLogeadoException();
+        }
         return "registrados";
     }
 }
